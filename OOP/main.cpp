70232199805214d7f6cc2a80737a5226cca10668
bunter12 <home52690@gmail.com>
2025-09-02 10:38:21 +0300

    Initial Commit

---
+++ OOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP
+//
+//  Created by Кирам Сабирзянов on 02.09.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
